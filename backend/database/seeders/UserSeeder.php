505aa3e87d1b65d329f5ad47d68dd00a4a22673a
nasubi64 <nasumonagamori@yahoo.co.jp>
2026-07-16 20:52:02 +0900

    feat: expand demo media seed data

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -21,6 +21,21 @@ class UserSeeder extends Seeder
                 'email'    => 'ren@example.com',
                 'password' => 'password',
             ],
+            [
+                'name'     => '佐藤 葵',
+                'email'    => 'aoi@example.com',
+                'password' => 'password',
+            ],
+            [
+                'name'     => '鈴木 湊',
+                'email'    => 'minato@example.com',
+                'password' => 'password',
+            ],
+            [
+                'name'     => '高橋 凛',
+                'email'    => 'rin@example.com',
+                'password' => 'password',
+            ],
         ];
 
         foreach ($users as $data) {
